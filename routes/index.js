fb515379d85b4cbf8ea66615c3330aa887d30b2c
Rully_H-F <Rullyhidayatullahfaizal@gmail.com>
2024-05-04 12:54:13 +0700

    auth jwt

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getUsers, login, register,Logout } from "../controllers/users.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { refreshToken } from "../controllers/refreshToken.js";
+const router = express.Router();
+
+router.get('/users',verifyToken,getUsers);
+router.post('/users',register);
+router.post('/login',login);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
+
+
+
+
+export default router;
